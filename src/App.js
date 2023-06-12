b9624c914ef2c4f3ce6c2d3b2be6171bbeebcbd4
kopever <kop.still@gmail.com>
2023-06-12 20:25:18 +0800

    lifting state up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Board() {
+function Square({ value, onSquareClick }) {
   return (
-    <React.Fragment>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
-    </React.Fragment>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquare = squares.slice();
+    nextSquare[i] = "X";
+    setSquares(nextSquare);
+  }
+
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </React.Fragment>
+  );
 }
