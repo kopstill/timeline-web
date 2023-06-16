720966646cf6555452c41399b6daf05c6be7e681
kopever <kop.still@gmail.com>
2023-06-16 17:52:06 +0800

    filterable product table

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import TicTacToe from "./TicTacToe";
+import FilterableProductTable from "./FilterableProductTable";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <TicTacToe />
+    <FilterableProductTable />
   </StrictMode>
 );
